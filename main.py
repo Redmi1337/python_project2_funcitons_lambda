09265146c91f6fb9c6e605002fb8bc6e1aadeb77
Redmi <radmirvasilevskyi@gmail.com>
2026-07-04 19:09:23 +0300

    Added a few comments

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 def main():
     foo(5, b=4)
     count(1,2,3,4,5)
-    func(a=3, b=5, function=lambda a, b: a * b)
+    func(a=3, b=5, function=lambda a, b: a * b) #oh dude i love lambda funcitons
     return
 
-def foo(a, /, *, b):
+def foo(a, /, *, b): #these things are cool
     print(a+b)
     return
 
-def count(*numbers):
-    for x in numbers:
+def count(*numbers):    #as this is cool too, i learned i could do **var
+    for x in numbers:   #to make them dictionaries instead of tupples but its too early for that
         print(x)
     return
 
